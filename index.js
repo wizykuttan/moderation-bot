fd9dcdd414894229b10ca586eec70c449a9735f2
wizykuttan <annanambi89@gmail.com>
2026-05-27 19:07:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-setInterval(() => {
-  console.log("Bot running...");
-}, 5000);
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("Bot running");
+  res.end();
+}).listen(3000);
+
+console.log("Server started");
